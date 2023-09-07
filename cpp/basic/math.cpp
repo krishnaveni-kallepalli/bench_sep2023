72e8c435176c835a3f993330fbbfc16a0156a18e
krish <krishnaveni.k@thundersoft.com>
2023-09-07 17:09:41 +0530

    07/09/23

---
+++ cpp/basic/math.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<cmath>	
+using namespace std;
+int main()
+{
+	cout << "max(2,3) : " << max(2,3) << endl;
+	cout << "min(2,3) : " << min(2,3) << endl;
+	cout << "sqrt(2)  : " << sqrt(2) << endl;
+	cout << "pow(2,3) : " << pow(2,3) << endl;
+	cout << "log(2)   : " << log(2) << endl;
+	cout << "round(3.44) : " << round(3.44) << endl;
+	cout << "floor(3.44) : " << floor(3.44) << endl;
+	cout << sqrt(sin(30)) + sqrt(cos(30)) << endl;
+	return 0;
+}
--- cpp/basic_programs/prime.cpp
+++ cpp/basic_programs/prime.cpp
@@ -5,15 +5,11 @@ int main()
 	int num,flag=0,i;
 	cout << "Enter num:";
 	cin >> num;
-	if(num==0|num==1) 
-		flag ++;
+	if(num==0|num==1) flag ++;
 	else
 	{
 		for(i=2;i<num;i++)
-		{
-			if(num%i==0)
-				flag++;
-		}
+			if(num%i==0) flag++;
 	}
 	if(flag==0) cout << num << " is primne\n";
 	else cout << num << " is composite\n";
